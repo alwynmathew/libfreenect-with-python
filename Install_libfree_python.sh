86c3330f258f83967e9276d4c937ed5adb543be8
Alwyn Mathew <alwynmathew.90@gmail.com>
2017-12-30 18:10:38 +0530

    Update Install_libfree_python.sh

--- Install_libfree_python.sh
+++ Install_libfree_python.sh
@@ -4,9 +4,11 @@
 #
 # Purpose : Install Libfreenect wrapper for python on Ubuntu 16.04
 
+sudo apt-get install python-pip python-dev build-essential 
 sudo apt-get install cython
 sudo apt-get install python-dev
 sudo apt-get install python-numpy
+sudo pip install opencv-python
 
 cd libfreenect/wrappers/python
 
